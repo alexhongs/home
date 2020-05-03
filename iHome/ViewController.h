aa4faf5d55fa4ae4af22de118255a78a8739fad9
Alex Hong <seongkwh@andrew.cmu.edu>
2020-05-03 03:00:02 -0400

    initial commit

---
+++ iHome/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iHome
+//
+//  Created by Alex Hong on 5/2/20.
+//  Copyright © 2020 alexhongs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <HomeKit/HomeKit.h>
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ iHome/ViewController.m
@@ -0,0 +1,25 @@
+//
+//  ViewController.m
+//  iHome
+//
+//  Created by Alex Hong on 5/2/20.
+//  Copyright © 2020 alexhongs. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    
+    
+}
+
+@end
