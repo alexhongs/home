916ec0b5a98de51c6ae96b395d6f3dac8bf954cc
Alex Hong <seongkwh@andrew.cmu.edu>
2020-05-19 16:08:44 -0400

    Implemented Room List View

---
+++ iHome/RoomViewController.h
@@ -0,0 +1,17 @@
+//
+//  RoomViewController.h
+//  iHome
+//
+//  Created by Alex Hong on 5/19/20.
+//  Copyright © 2020 alexhongs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <HomeKit/HomeKit.h>
+NS_ASSUME_NONNULL_BEGIN
+
+@interface RoomViewController : UIViewController<UICollectionViewDelegate, UICollectionViewDataSource>
+
+@end
+
+NS_ASSUME_NONNULL_END
--- iHome/RootViewController.h
+++ iHome/RootViewController.h
@@ -12,12 +12,6 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface RootViewController : UIViewController<HMHomeManagerDelegate, HMHomeDelegate, UICollectionViewDelegate, UICollectionViewDataSource>
 
-@property (nonatomic, strong) HMHomeManager *homeManager;
-@property (nonatomic, strong) HMHome *home;
-@property (nonatomic, strong) NSArray<HMHome *> *homes;
-
-@property (strong, nonatomic) IBOutlet UICollectionView *collectionView;
-
 @end
 
 NS_ASSUME_NONNULL_END
