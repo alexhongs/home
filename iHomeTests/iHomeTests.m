621c271d643d9e3bc944a7078096339951d7a78d
Alex Hong <seongkwh@andrew.cmu.edu>
2020-06-03 18:52:49 -0400

    test framework added

---
+++ iHomeTests/iHomeTests.m
@@ -0,0 +1,42 @@
+//
+//  iHomeTests.m
+//  iHomeTests
+//
+//  Created by Alex Hong on 6/1/20.
+//  Copyright © 2020 alexhongs. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+#import "../iHome/RoomView/RoomViewController.h"
+
+//#import "RoomViewController.h"
+
+@interface iHomeTests : XCTestCase
+
+@end
+
+@implementation iHomeTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+//- (void)testRoomViewController
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
